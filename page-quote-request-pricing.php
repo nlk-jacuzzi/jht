57894c24138b0fcfedf3113ccce707f86c6b7988
Tim Spinks <tim@ninthlink.com>
2015-03-20 13:01:07 -0700

    more css fixes

--- page-quote-request-pricing.php
+++ page-quote-request-pricing.php
@@ -61,7 +61,7 @@ if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
     <div class="bd request-pricing">
     	<div class="wrap">
             <div class="twoCol">
-                <div class="main request-pricing" style="display: none;">
+                <div class="main request-pricing" style="visibility: hidden;">
                     <h1 class="title"><?php the_title(); ?></h1>
                     <p>Simply fill in this quick form to request pricing on your perfect hot tub. Your local authorized Jacuzzi dealer will reach out to you with expert selection advice, pricing, and any current specials in your area.<br><br>*Indicates required fields.</p>
 					<?php echo do_shortcode('[gravityform id="14" name="No-Obligation Price Quote" title="false" description="false"]'); ?>
@@ -74,8 +74,8 @@ if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
                     var opt = $('.avalaFieldProductId option').filter(function () { return $(this).html() == sel; }).val();
                     $('.avalaFieldProductId option[value="'+opt+'"]').attr('selected', 'selected');
                 })(jQuery);
-                jQuery( window ).load(function($){
-                    $('div.main.request-pricing').show();
+                jQuery('div.main.request-pricing').load(function($){
+                    $('div.main.request-pricing').css('visibility', 'visible');
                 });
                 </script>
                 <div class="side">
