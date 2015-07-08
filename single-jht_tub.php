7978bbf4b372b522d50785625c91f1509eb3e586
Tim Spinks <tim@ninthlink.com>
2015-07-08 10:04:50 -0700

    JAC-686 updated seo title with pregreplace for unwanted chars

--- single-jht_tub.php
+++ single-jht_tub.php
@@ -144,7 +144,7 @@ $bv = new BV(
                         </div>
                     </div>
                     <div class="side">
-                        <meta itemprop="name" content="<?php echo the_title(); ?>" />
+                        <meta itemprop="name" content="<?php echo preg_replace( "/[^A-Za-z0-9 \-]/", '', the_title() ); ?>" />
                         <h1><?php the_title(); ?></h1>
                         <h2><?php esc_attr_e($jht_info['topheadline']); ?></h2>
                             <div id="BVRRSummaryContainer"></div>
