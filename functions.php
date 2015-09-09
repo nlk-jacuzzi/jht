3bfaebddac9e9ba83eeace363f660b7b043aa60f
Tim Spinks <tim@ninthlink.com>
2015-09-09 10:25:57 -0700

    copy updates

--- functions.php
+++ functions.php
@@ -1526,7 +1526,7 @@ function jht_specs_metabox() {
 	</td>
     </tr> */ ?>
     <tr valign="top">
-    <td width="187"><label for="jht_specs[wiz_bullets]">Wizard Bullet Points<br /><small>One per line. No HTML or list markers (for example - or •).</small></label></td><td><textarea name="jht_specs[wiz_bullets]" cols="120" rows="5"><?php esc_attr_e($info['wiz_bullets']); ?></textarea></td>
+    <td width="187"><label for="jht_specs[wiz_bullets]">Wizard Bullet Points<br /><small>One per line. No list markers (for example - or •).</small></label></td><td><textarea name="jht_specs[wiz_bullets]" cols="120" rows="5"><?php esc_attr_e($info['wiz_bullets']); ?></textarea></td>
     </tr>
     <tr><td colspan="2"><p><strong>Featured Image Details</strong></p></td></tr>
     <tr valign="top">
