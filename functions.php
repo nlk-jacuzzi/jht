e2d74994dca5fd2a32f81eafe2d17c683297e193
AmitDeo <magpie.arise@gmail.com>
2015-10-02 14:26:04 +0545

    Blank bullet point fix

--- functions.php
+++ functions.php
@@ -4161,12 +4161,10 @@ function custom_confirmation( $confirmation, $form, $entry, $ajax ) {
 			$confirmation .= '<div class="wiz-bullets">
 					<ul>';
 			foreach ($wiz_bullets as $wiz_bullet) {
-<<<<<<< HEAD
-				$confirmation .= '<li>'.html_entity_decode($wiz_bullet).'</li>';
-=======
+					
 				if(trim($wiz_bullet) != '')
-					$confirmation .= '<li>'.$wiz_bullet.'</li>';
->>>>>>> JAC-775
+					$confirmation .= '<li>'.html_entity_decode($wiz_bullet).'</li>';
+				
 			}
 			$confirmation .= '<ul>
 				</div>
