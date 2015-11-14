4706a7263ff83a3536612be1b61990413c9af739
Tim Spinks <tim@ninthlink.com>
2015-11-14 10:59:49 -0800

    JAC-1142 added ajax call

--- page-jacuzzi-brochure-onepart.php
+++ page-jacuzzi-brochure-onepart.php
@@ -206,7 +206,7 @@ if ( have_posts() ) while ( have_posts() ) : the_post();
             	<h1><strong class="small">THE JACUZZI&reg; HOT TUB BROCHURE </strong>25 Pages of Facts &amp; Photos - <strong>Free</strong></h1>
                 <h2>Get instant access to Jacuzzi&reg; Hot Tub features , stunning lifestyle photos,<br />collection descriptions and more.</h2>
             </div>
-            <?php echo do_shortcode('[gravityform id="13" name="Download a Free Jacuzi Hot Tub Brochure Today" title="false" description="false"]'); ?>
+            <?php echo do_shortcode('[gravityform id="13" name="Download a Free Jacuzi Hot Tub Brochure Today" title="false" description="false" ajax="true"]'); ?>
             <p style="position: absolute; right: 167px; bottom: 87px;"><small><i>* indicates required field<br><a href="<?php echo get_permalink(3987) ?>">Privacy policy.</a></i></small></p>
             <a id="click-me-anchor" href="#more-info"></a>
 
